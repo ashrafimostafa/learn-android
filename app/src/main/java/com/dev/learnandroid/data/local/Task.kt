226adeab633910de6bd587a53efc1693ea7a6ce9
Mostafa <ashrafi.m@yaftar.ir>
2022-02-05 23:48:46 +0330

    ui added

---
+++ app/src/main/java/com/dev/learnandroid/data/local/Task.kt
@@ -0,0 +1,20 @@
+package com.dev.learnandroid.data.local
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
+
+@Entity(tableName = "TaskTable")
+@Parcelize
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val isChecked: Boolean = false,
+    val isImportant: Boolean = false,
+    val createdDate: Long = System.currentTimeMillis()
+) : Parcelable {
+    val createdDateFormatted: String
+        get() = DateFormat.getDateTimeInstance().format(createdDate)
+}
